3fc47f26623e41b0e61a7e5d505dc9add0f9babd
Ahmad Dzulfikar Ubaidillah <dzulfprojects@gmail.com>
2023-04-12 00:23:37 +0700

    write more file

--- sezero/settings.py
+++ sezero/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-p_3mj-#8($%q&^^2x^kyg$@%s3*73&a9c^h1pyyalsgof1k6kj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
