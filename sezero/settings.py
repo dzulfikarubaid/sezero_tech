6e0fe9cfc5562227721f9dd8e15a9b7d42690e86
Ahmad Dzulfikar Ubaidillah <dzulfprojects@gmail.com>
2023-04-20 21:52:56 +0700

    write more file

--- sezero/settings.py
+++ sezero/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p_3mj-#8($%q&^^2x^kyg$@%s3*73&a9c^h1pyyalsgof1k6kj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
